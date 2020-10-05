7fff057377a9b229856f6d227009edbec0874bcc
Luis Gutiérrez <luisantonio.gutierrezhernandez@thosmsonreuters.com>
2020-10-05 15:58:23 -0500

    Change message. Selete literal teplate

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 function run() {
-  core.info(`These are the context content:`);
-  core.info(`${JSON.stringify(github.context, null, 4)}`);
+  core.info(`This is the context content:`);
+  core.info(JSON.stringify(github.context, null, 4));
 }
 
 run();
