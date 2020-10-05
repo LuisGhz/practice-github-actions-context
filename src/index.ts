4ca9979f1c5be5c5311ecb17740fd3ba42d3c61c
Luis Gutiérrez <luisantonio.gutierrezhernandez@thosmsonreuters.com>
2020-10-05 12:42:44 -0500

    Install dependencies & use context

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 function run() {
-  const name: string = core.getInput('my_input');
-  if (name) {
-    core.debug(`Hello ${name}!`);
-    return core.setOutput('my_output', `Hello ${name}!`);
-  }
-  core.setFailed('my_input not specified!');
+  core.info(`These are the context content:`);
+  core.info(`${github.context}`);
 }
 
 run();
